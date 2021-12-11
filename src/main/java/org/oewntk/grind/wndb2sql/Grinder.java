a489f1a17b4398fb1b49c611f1e480b9df86858a
1313ou <1313ou@gmail.com>
2021-12-11 21:06:51 +0100

    Initial

---
+++ src/main/java/org/oewntk/grind/wndb2sql/Grinder.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2021-2021. Bernard Bou.
+ */
+
+package org.oewntk.grind.wndb2sql;
+
+/**
+ * Main class that generates the WN database in the SQL format from the WNDB format
+ *
+ * @author Bernard Bou
+ */
+public class Grinder
+{
+	/**
+	 * Main entry point
+	 *
+	 * @param args command-line arguments
+	 */
+	public static void main(String[] args)
+	{
+	}
+}
