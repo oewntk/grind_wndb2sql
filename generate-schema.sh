7b2c7a6206511ab3ffc08e3226aee51a4855c648
1313ou <1313ou@gmail.com>
2022-03-15 19:44:43 +0100

    table renames

--- generate-schema.sh
+++ generate-schema.sh
@@ -8,7 +8,13 @@
 # indir
 # files*: external sql template file names, if none they are taken from resources
 
-source define_colors.sh
+export R='\u001b[31m'
+export G='\u001b[32m'
+export B='\u001b[34m'
+export Y='\u001b[33m'
+export M='\u001b[35m'
+export C='\u001b[36m'
+export Z='\u001b[0m'
 
 if [ "$1" == "-compat" ]; then
   compatswitch="-compat"
