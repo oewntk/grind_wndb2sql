c3c69a7a68852a01d8d3f4e4db0e2e9e5c3377c5
1313ou <1313ou@gmail.com>
2024-04-29 19:44:09 +0200

    reformat

--- src/main/java/org/oewntk/grind/wndb2sql/Grind.kt
+++ src/main/java/org/oewntk/grind/wndb2sql/Grind.kt
@@ -14,45 +14,45 @@ import java.io.File
  */
 object Grind {
 
-	/**
-	 * Main entry point
-	 *
-	 * @param args command-line arguments
-	 */
-	@JvmStatic
-	fun main(args: Array<String>) {
-		val iArg = 0
+    /**
+     * Main entry point
+     *
+     * @param args command-line arguments
+     */
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val iArg = 0
 
-		// Tracing
-		val startTime = Tracing.start()
+        // Tracing
+        val startTime = Tracing.start()
 
-		// Input
-		val inDir = File(args[iArg])
-		Tracing.psInfo.println("[Input] " + inDir.absolutePath)
+        // Input
+        val inDir = File(args[iArg])
+        Tracing.psInfo.println("[Input] " + inDir.absolutePath)
 
-		// Input2
-		val inDir2 = File(args[iArg + 1])
-		Tracing.psInfo.println("[Input2] " + inDir2.absolutePath)
+        // Input2
+        val inDir2 = File(args[iArg + 1])
+        Tracing.psInfo.println("[Input2] " + inDir2.absolutePath)
 
-		// Output
-		val outDir = File(args[iArg + 2])
-		if (!outDir.exists()) {
-			outDir.mkdirs()
-		}
-		Tracing.psInfo.println("[Output] " + outDir.absolutePath)
+        // Output
+        val outDir = File(args[iArg + 2])
+        if (!outDir.exists()) {
+            outDir.mkdirs()
+        }
+        Tracing.psInfo.println("[Output] " + outDir.absolutePath)
 
-		// Supply model
-		Tracing.progress("before model is supplied,", startTime)
-		val model = Factory(inDir, inDir2).get()
-		//Tracing.psInfo.printf("[Model] %s%n%s%n%n", Arrays.toString(model.getSources()), model.info())
-		Tracing.progress("after model is supplied,", startTime)
+        // Supply model
+        Tracing.progress("before model is supplied,", startTime)
+        val model = Factory(inDir, inDir2).get()
+        //Tracing.psInfo.printf("[Model] %s%n%s%n%n", Arrays.toString(model.getSources()), model.info())
+        Tracing.progress("after model is supplied,", startTime)
 
-		// Consume model
-		Tracing.progress("before model is consumed,", startTime)
-		ModelConsumer(outDir).accept(model!!)
-		Tracing.progress("after model is consumed,", startTime)
+        // Consume model
+        Tracing.progress("before model is consumed,", startTime)
+        ModelConsumer(outDir).accept(model!!)
+        Tracing.progress("after model is consumed,", startTime)
 
-		// End
-		Tracing.progress("total,", startTime)
-	}
+        // End
+        Tracing.progress("total,", startTime)
+    }
 }
