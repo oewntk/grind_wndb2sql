85c4eb02f66a5aded7f9ccb2689a0df3beba21fd
1313ou <1313ou@gmail.com>
2026-06-01 11:53:44 +0200

    organize serializations

--- src/main/java/org/oewntk/grind/wndb2sql/Serializer.kt
+++ src/main/java/org/oewntk/grind/wndb2sql/Serializer.kt
@@ -3,7 +3,7 @@
  */
 package org.oewntk.grind.wndb2sql
 
-import org.oewntk.model.Serialize.serializeCoreModel
+import org.oewntk.model.SerializeJVM.serializeCoreModel
 import org.oewntk.sql.out.SerializeNIDs.serializeNIDs
 import org.oewntk.wndb.`in`.Factory.Companion.makeModel
 import java.io.File
