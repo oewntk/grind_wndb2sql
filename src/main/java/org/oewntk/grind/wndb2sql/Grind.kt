7780c5077f645f3ccdb98ac11f98cd61edc194c0
1313ou <1313ou@gmail.com>
2026-06-07 18:10:15 +0200

    better logginf

--- src/main/java/org/oewntk/grind/wndb2sql/Grind.kt
+++ src/main/java/org/oewntk/grind/wndb2sql/Grind.kt
@@ -6,6 +6,7 @@ package org.oewntk.grind.wndb2sql
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.default
+import org.oewntk.grind.wndb2sql.Tracing.progress
 import org.oewntk.sql.out.ModelConsumer
 import org.oewntk.wndb.`in`.Factory
 import java.io.File
@@ -59,16 +60,16 @@ object Grind {
         Tracing.psInfo.println("[Output] " + outDir.absolutePath)
 
         // Supply model
-        Tracing.progress("before model is supplied,", startTime)
+        progress("before model is supplied", startTime)
         val model = Factory(inDir, inDir2, verbose = verbose).get()
-        Tracing.progress("after model is supplied,", startTime)
+        progress("after model is supplied", startTime)
 
         // Consume model
-        Tracing.progress("before model is consumed,", startTime)
+        progress("before model is consumed", startTime)
         ModelConsumer(outDir, verbose = verbose).accept(model!!)
-        Tracing.progress("after model is consumed,", startTime)
+        progress("after model is consumed", startTime)
 
         // End
-        Tracing.progress("total,", startTime)
+        progress("end", startTime)
     }
 }
